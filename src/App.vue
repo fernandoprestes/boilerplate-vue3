25079b2bd3c91597144a5ff7b874bcede41ced73
fernandoprestes <fernandoprestesc@gmail.com>
2022-03-17 15:59:18 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <h1 class="text-2xl text-purple-900">HelloWorld</h1>
+  </div>
+</template>
+<script lang="ts">
+  export default {};
+</script>
+<style lang=""></style>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,vue}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
