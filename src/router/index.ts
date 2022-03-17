6f41bfb6290347159ed6025a3b96bc39cb07691f
fernandoprestes <fernandoprestesc@gmail.com>
2022-03-17 18:45:33 -0300

    add router-vue and example page

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import HomePage from '../views/HomePage.vue';
+
+const route: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomePage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+
+  routes: route,
+});
+
+export default router;
---
+++ src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div class="grid h-screen place-items-center">
+    <h1 class="text-2xl font-bold">Hello World</h1>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'HomePage',
+  });
+</script>
+<style lang=""></style>
