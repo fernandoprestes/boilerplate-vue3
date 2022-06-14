87b92e46221462f74a7063cc0f200cd8f4834031
fernandoprestes <fernandoprestesc@gmail.com>
2022-06-14 20:30:42 -0300

    add autoimport componets

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,9 @@
+<script lang="ts" setup></script>
 <template lang="">
   <div class="grid h-screen place-items-center">
     <h1 class="text-2xl font-bold">Hello World</h1>
+    <teste />
   </div>
 </template>
-<script lang="ts">
-  import { defineComponent } from 'vue';
 
-  export default defineComponent({
-    name: 'HomePage',
-  });
-</script>
 <style lang=""></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()]
-})
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+
+  plugins: [
+    vue(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: ['vue', 'vue-router', '@vueuse/head', '@vueuse/core'],
+      dts: 'src/auto-imports.d.ts',
+      dirs: ['src/composables', 'src/store'],
+      vueTemplate: true,
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      extensions: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: 'src/components.d.ts',
+    }),
+  ],
+});
