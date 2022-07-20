353de50bcebf0f9eb47202e22e99f76ece1b53a5
fernandoprestes <fernandoprestesc@gmail.com>
2022-07-20 18:23:35 -0300

    feat: add unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig, presetTypography, presetUno, presetWebFonts } from 'unocss';
+import { defineConfig, presetTypography, presetAttributify, presetUno, presetWebFonts } from 'unocss';
 import presetRemToPx from '@unocss/preset-rem-to-px';
 import presetIcons from '@unocss/preset-icons';
 
 export default defineConfig({
+  theme: {
+    colors: {
+      primary: '#294A3C',
+    },
+  },
   shortcuts: [
     ['container-center', 'container mx-auto px-4'],
     ['text-gradient', 'bg-gradient-to-rt from-[#294A3C] via-[#60917C] to-[#96D7BB] bg-clip-text text-transparent'],
   ],
   presets: [
     presetUno(),
-    presetTypography(),
     presetWebFonts({
+      provider: 'google',
       fonts: {
         sans: ['Roboto', 'sans'],
-        serif: 'DM Serif Display',
         mono: 'DM Mono',
         hand: ['Dancing Script', 'cursive'],
       },
     }),
+    presetAttributify(),
+    presetTypography(),
     presetIcons(),
     presetRemToPx(),
   ],
